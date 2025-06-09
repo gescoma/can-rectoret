6dd4e68c4d851ef52af14ba691ce14082ca28659
Gerard Escolies <gerard.esma@icloud.com>
2025-06-09 22:45:36 +0200

    refactor: reorganize imports and adjust layout in HighImpactHero component

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
-import React, { useEffect } from 'react'
 
-import type { Page } from '@/payload-types'
+import React, { useEffect } from 'react'
 
 import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
+import type { Page } from '@/payload-types'
 import RichText from '@/components/RichText'
+import { useHeaderTheme } from '@/providers/HeaderTheme'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   const { setHeaderTheme } = useHeaderTheme()
@@ -17,10 +17,9 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
 
   return (
     <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
+      className="relative -mt-16 flex items-center justify-center text-white container mb-8 z-10 rounded-2xl overflow-hidden"
       data-theme="dark"
     >
-      <div className="container mb-8 z-10 relative flex items-center justify-center">
         <div className="max-w-[36.5rem] md:text-center">
           {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
@@ -35,7 +34,6 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
             </ul>
           )}
         </div>
-      </div>
       <div className="min-h-[80vh] select-none">
         {media && typeof media === 'object' && (
           <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
